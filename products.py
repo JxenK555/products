67244295aeb0e413a834aebf5f9f4b3312401c8b
Jxen Khor <jxenkhor@gmail.com>
2020-12-03 16:23:07 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,15 @@
+#读取档案
 products = []
+with open('products.csv', 'r', encoding='utf-8')as f:
+	for line in f:
+		if 'Products, Price' in line:
+			continue   #继续
+		name, price = line.strip().split(',')
+		products.append([name, price])
+		
+print(products)
+
+
 while True:
 	name = input('Insert Product Name: ')
 	if name == 'q':
@@ -9,9 +20,9 @@ while True:
 print(products)	
 
 for p in products:
-	print('Product:', p[0], 'Price:', p[1])
+	print('Product:', p[0], '  Price:', p[1])
 
 with open('products.csv', 'w', encoding='utf-8')as f:
-	f.write('商品, 价钱\n')
+	f.write('Products, Price\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
