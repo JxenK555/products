bdf83986ac507ef0be893296f14e29877c96aff7
Jxen Khor <jxenkhor@gmail.com>
2020-12-03 16:58:44 +0800

    add check file

--- products.py
+++ products.py
@@ -1,15 +1,21 @@
-#读取档案
+import os #operating system
+
+#讀取檔案
 products = []
-with open('products.csv', 'r', encoding='utf-8')as f:
-	for line in f:
-		if 'Products, Price' in line:
-			continue   #继续
-		name, price = line.strip().split(',')
-		products.append([name, price])
-		
-print(products)
+if os.path.isfile('products.csv'):   #檢查檔案在不在
+	print('This file has been found.')
+	with open('products.csv', 'r', encoding='utf-8')as f:
+		for line in f:
+			if 'Products, Price' in line:
+				continue   #继续
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('Sorry, I dont see this file!')
+	print('Type in the blank to create a new file')
 
-
+#让使用者输入
 while True:
 	name = input('Insert Product Name: ')
 	if name == 'q':
@@ -19,9 +25,11 @@ while True:
 	products.append([name, price])
 print(products)	
 
+#印出所有购买记录
 for p in products:
 	print('Product:', p[0], '  Price:', p[1])
 
+#写入档案
 with open('products.csv', 'w', encoding='utf-8')as f:
 	f.write('Products, Price\n')
 	for p in products:
